335b68daad9b89dc198fc60a7f741745d6e2346d
Marto_0 <martvanenck1@gmail.com>
2024-01-16 10:01:51 +0100

    whoops

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,4 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
   preprocess: vitePreprocess(),
-  base: '/belts',
-  kit: {
-    paths: {
-      base: '/belts/',
-    }
-  }
 }
